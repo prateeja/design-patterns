30e004647e53ce0902c6a1c5e59c0f0a2a9be6f1
Prateek Jain <prateekjainit@gmail.com>
2020-05-25 09:55:40 +0530

    Design pattern multi module inital setup

---
+++ behavioral-design-patterns/visitor-design-pattern/src/main/java/com/designpatterns/behavioral/visitor/App.java
@@ -0,0 +1,13 @@
+package com.designpatterns.behavioral.visitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ structural-design-patterns/bridge-design-pattern/src/main/java/com/designpatterns/structural/bridge/App.java
@@ -0,0 +1,13 @@
+package com.designpatterns.structural.bridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
